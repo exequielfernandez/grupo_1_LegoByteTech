71f9c50e8023615693a7445538ccadae27433435
Gustavo <gustavodanielgb@gmail.com>
2024-07-02 02:49:37 -0300

    Corrijo script del slider

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,20 +1,22 @@
 //Script para el Slider del Home
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', function() {
+    const productContainers = document.querySelectorAll('.product-container');
+    const nxtBtn = document.querySelector('.nxt-btn');
+    const preBtn = document.querySelector('.pre-btn');
+  
+    productContainers.forEach(item => {
+      let containerDimensions = item.getBoundingClientRect();
+      let containerWidth = containerDimensions.width;
+  
+      nxtBtn.addEventListener('click', () => {
         item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
+      });
+  
+      preBtn.addEventListener('click', () => {
         item.scrollLeft -= containerWidth;
-    })
-})
+      });
+    });
+  });
 //Script para el menú de la barra de navegación
 function toggleMenu() {
     const navList = document.querySelector('nav ul');
