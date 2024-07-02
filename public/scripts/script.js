73c69443b86ca6347afbe5d543ce276b48f554a1
Gustavo <gustavodanielgb@gmail.com>
2024-07-02 02:00:03 -0300

    Agrego script.js

---
+++ public/scripts/script.js
@@ -0,0 +1,22 @@
+//Script para el Slider del Home
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+//Script para el menú de la barra de navegación
+function toggleMenu() {
+    const navList = document.querySelector('nav ul');
+    navList.classList.toggle('active');
+}
