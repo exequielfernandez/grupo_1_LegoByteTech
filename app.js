c02baaf4ac5ac9fd68640b4d6ddb2783f82f735c
MLM32 <marcosluismontilla@gmail.com>
2024-07-17 20:37:39 -0300

    implementacion de ejs y renombre de vistas

--- app.js
+++ app.js
@@ -1,61 +1,38 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 // Middleware para servir archivos estáticos desde la carpeta 'public'
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
 
 // Escuchar en el puerto definido
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
 // Ruta Home
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "home.html"), (err) => {
-        if (err) {
-            console.error(`Error al enviar el archivo home.html: ${err}`);
-            res.status(err.status || 500).send('Error interno del servidor');
-        }
-    });
+  res.render("home");
 });
 // Ruta Carrito
 app.get("/cart", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "cart.html"), (err) => {
-        if (err) {
-            console.error(`Error al enviar el archivo cart.html: ${err}`);
-            res.status(err.status || 500).send('Error interno del servidor');
-        }
-    });
+  res.render("cart");
 });
 // Ruta login
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "login.html"), (err) => {
-        if (err) {
-            console.error(`Error al enviar el archivo login.html: ${err}`);
-        }
-    });
+  res.render("login");
 });
 
 // Ruta productDetail
 app.get("/productDetail", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "productDetail.html"), (err) => {
-        if (err) {
-            console.error(`Error al enviar el archivo productDetail.html: ${err}`);
-            res.status(err.status || 500).send('Error interno del servidor');
-        }
-    });
+  res.render("productDetail");
 });
 
 // Ruta signUp
 app.get("/signUp", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "signUp.html"), (err) => {
-        if (err) {
-            console.error(`Error al enviar el archivo signUp.html: ${err}`);
-            res.status(err.status || 500).send('Error interno del servidor');
-        }
-    });
+  res.render("singUp");
 });
-
